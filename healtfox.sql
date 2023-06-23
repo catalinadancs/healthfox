5e8a5826b3cc7af8d9875948ca24abb133559071
Catalina Dancs <catalinadancs@gmail.com>
2023-06-23 13:09:37 +0300

    Update healtfox.sql

--- healtfox.sql
+++ healtfox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2023 at 07:31 PM
+-- Generation Time: Jun 23, 2023 at 12:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
@@ -41,6 +41,33 @@ INSERT INTO `doctori` (`id`, `nume`, `prenume`, `specializare`, `clinica`, `loca
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rezultate`
+--
+
+CREATE TABLE `rezultate` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `hypertension` int(11) DEFAULT NULL,
+  `heart_disease` int(11) DEFAULT NULL,
+  `smoking_history` int(11) DEFAULT NULL,
+  `bmi` float DEFAULT NULL,
+  `hba1c_level` float DEFAULT NULL,
+  `blood_glucose_level` float DEFAULT NULL,
+  `prediction` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rezultate`
+--
+
+INSERT INTO `rezultate` (`id`, `user_id`, `gender`, `age`, `hypertension`, `heart_disease`, `smoking_history`, `bmi`, `hba1c_level`, `blood_glucose_level`, `prediction`) VALUES
+(1, 10, 1, 23, 0, 0, 0, 26, NULL, 90, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -61,7 +88,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `nume`, `prenume`, `username`, `email`, `parola`, `utype`) VALUES
 (1, 'Dancs', 'Catalina', 'dnxcatalina', 'catalinadancs@gmail.com', 'brutus', 'admin'),
 (3, 'da', 'da', 'da', 'da', 'da', 'admin'),
-(8, 'brutus', 'brutus', 'brutuss', 'brutus@gmail.com', 'brutus', 'admin'),
+(8, 'brutus', 'brutus', 'brutus', 'brutus@gmail.com', 'brutus', 'admin'),
 (10, 'dada', 'dada', 'dada1', 'dadaff@dd', '1234', 'user');
 
 --
@@ -72,6 +99,12 @@ INSERT INTO `users` (`id`, `nume`, `prenume`, `username`, `email`, `parola`, `ut
 -- Indexes for table `doctori`
 --
 ALTER TABLE `doctori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rezultate`
+--
+ALTER TABLE `rezultate`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,6 +122,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `doctori`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4334;
+
+--
+-- AUTO_INCREMENT for table `rezultate`
+--
+ALTER TABLE `rezultate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
